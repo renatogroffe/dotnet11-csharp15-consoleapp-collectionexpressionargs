11fd6c621cd184555ef0232fa5ad161e35309654
Renato Groffe <renato.groffe@yahoo.com.br>
2026-02-18 21:48:50 -0300

    Commit inicial

---
+++ src/ConsoleAppCollectionExpression/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+
+Console.WriteLine("***** Testes com .NET 11 + C# 15 | IReadOnlySet<T> + Collection expression arguments *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+Console.WriteLine();
+
+string[] paises = ["Brasil", "Italia", "Portugal", "Alemanha",
+    "ITALIA", "Espanha", "BRASIL", "portugal", "espanha", "AlEmAnHA", "Inglaterra"]; 
+
+List<string> listPaises = [with(capacity: 20), .. paises];
+Console.WriteLine($"{nameof(listPaises)} = {JsonSerializer.Serialize(listPaises)}");
+
+Console.WriteLine();
+
+HashSet<string> hashSetPaises = [with(comparer: StringComparer.OrdinalIgnoreCase), .. listPaises];   
+Console.WriteLine($"{nameof(hashSetPaises)} = {JsonSerializer.Serialize(hashSetPaises)}");
