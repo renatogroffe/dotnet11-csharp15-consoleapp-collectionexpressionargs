782789ae2fbd9c770e25d8ead96caec217c53e09
Renato Groffe <renato.groffe@yahoo.com.br>
2026-02-20 23:31:40 -0300

    Ajustes

--- src/ConsoleAppCollectionExpression/Program.cs
+++ src/ConsoleAppCollectionExpression/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Runtime.InteropServices;
 using System.Text.Json;
 
-Console.WriteLine("***** Testes com .NET 11 + C# 15 | IReadOnlySet<T> + Collection expression arguments *****");
+Console.WriteLine("***** Testes com .NET 11 + C# 15 | Collection expression arguments *****");
 Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
     .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
     .OSVersion.VersionString}");
@@ -14,6 +14,5 @@ List<string> listPaises = [with(capacity: 20), .. paises];
 Console.WriteLine($"{nameof(listPaises)} = {JsonSerializer.Serialize(listPaises)}");
 
 Console.WriteLine();
-
 HashSet<string> hashSetPaises = [with(comparer: StringComparer.OrdinalIgnoreCase), .. listPaises];   
 Console.WriteLine($"{nameof(hashSetPaises)} = {JsonSerializer.Serialize(hashSetPaises)}");
